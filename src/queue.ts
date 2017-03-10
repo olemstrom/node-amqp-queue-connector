e7867f0d07c1adc042c8202b7a979964cc144ba6
Oscar Lemström <oscar.lemstrom@gmail.com>
2017-03-10 15:02:22 +0200

    Create queue class with useful default settings

--- src/queue.ts
+++ src/queue.ts
@@ -1,10 +1,6 @@
 import * as amqp from 'amqplib';
-import { Channel, Connection, Replies, Message } from 'amqplib'
+import { Channel, Message } from 'amqplib'
 import { BehaviorSubject, Subject, Observable } from 'rxjs/Rx'
-
-const openConnection: Promise<amqp.Connection> = amqp.connect(process.env.AMQP_URL);
-const channel: Promise<amqp.Channel> = openConnection.then((c: Connection): Promise<Channel> => c.createChannel());
-
 
 const sendToQueue = (topic: string, message: any, channel: Channel) => channel.assertQueue(topic).then(ok => channel.sendToQueue(topic, new Buffer(message)));
 
@@ -21,16 +17,36 @@ const consume = (topic: string, channel: Channel) => {
     return sub.asObservable();
 };
 
-export const publish = (topic: string, message: {}) => {
-    const send = sendToQueue.bind(null, topic, JSON.stringify(message));
-    return channel.then(send).catch(console.error);
-};
+export interface ExchangeOptions {
+    name: string;
+    type: 'direct' | 'fanout' | 'topic';
+}
 
-export const listen = (topic: string): Observable<any> => Observable.fromPromise(channel)
-        .switchMap(consume.bind(null, topic))
-        .filter(val => Boolean(val))
-        .catch((err: Error) => {
-            console.error(err);
-            return Observable.throw(err);
-        });
+export class AmqpConnector {
+    private channel: Promise<Channel>;
 
+    constructor(url: string, exchange: ExchangeOptions = { name: 'default-fanout', type: 'fanout'} ) {
+        this.channel = amqp.connect(url)
+            .then(c => c.createChannel)
+            .catch((error: Error) => {
+                throw error
+            });
+
+        this.channel.then(channel => channel.assertExchange(exchange.name, exchange.type))
+    }
+
+    publish(queue: string, message: any): Promise<any> {
+        const send = sendToQueue.bind(null, queue, JSON.stringify(message));
+        return this.channel.then(send).catch(console.error);
+    }
+
+    listen(queue: string): Observable<any> {
+        return Observable.fromPromise(this.channel)
+            .switchMap(consume.bind(null, queue))
+            .filter(val => Boolean(val))
+            .catch((err: Error) => {
+                console.error(err);
+                return Observable.throw(err);
+            });
+    }
+}
