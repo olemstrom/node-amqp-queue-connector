65da1702a2f858e78cb8fa48643427b7668793d7
Oscar Lemström <oscar.lemstrom@gmail.com>
2017-03-11 11:48:27 +0200

    Start utilising fanout exchange setup

--- main.d.ts
+++ main.d.ts
@@ -1,10 +1,12 @@
 import { Observable } from 'rxjs/Rx';
+export declare type ExchangeType = 'direct' | 'fanout' | 'topic';
 export interface ExchangeOptions {
     name: string;
-    type: 'direct' | 'fanout' | 'topic';
+    type: ExchangeType;
 }
 export declare class AmqpConnector {
     private channel;
+    private exchange;
     constructor(url: string, exchange?: ExchangeOptions);
     publish(queue: string, message: any): Promise<any>;
     listen(queue: string): Observable<any>;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import * as amqp from 'amqplib';
 import { Channel, Message } from 'amqplib'
 import { BehaviorSubject, Subject, Observable } from 'rxjs/Rx'
 
-const sendToQueue = (topic: string, message: any, channel: Channel) => channel.assertQueue(topic).then(ok => channel.sendToQueue(topic, new Buffer(message)));
+const initialiseQueue = (queue: string, exchange: string, channel: Channel) => Promise.all([
+    channel.assertQueue(queue),
+    channel.bindQueue(queue, exchange,'*')
+]);
+
+const sendToQueue = (queue: string, message: any, channel: Channel) => channel.sendToQueue(queue, new Buffer(message));
 
 const consume = (topic: string, channel: Channel) => {
     const sub: Subject<any> = new BehaviorSubject(undefined);
@@ -17,32 +22,42 @@ const consume = (topic: string, channel: Channel) => {
     return sub.asObservable();
 };
 
+export type ExchangeType = 'direct' | 'fanout' | 'topic';
+
 export interface ExchangeOptions {
     name: string;
-    type: 'direct' | 'fanout' | 'topic';
+    type: ExchangeType;
 }
 
 export class AmqpConnector {
     private channel: Promise<Channel>;
+    private exchange: string;
 
     constructor(url: string, exchange: ExchangeOptions = { name: 'default-fanout', type: 'fanout'} ) {
         this.channel = amqp.connect(url)
             .then(c => c.createChannel())
+            .then(channel => channel.assertExchange(exchange.name, exchange.type).then(() => channel))
             .catch((error: Error) => {
                 throw error
             });
 
-        this.channel.then(channel => channel.assertExchange(exchange.name, exchange.type))
+        this.exchange = exchange.name;
     }
 
     publish(queue: string, message: any): Promise<any> {
         const send = sendToQueue.bind(null, queue, JSON.stringify(message));
-        return this.channel.then(send).catch(console.error);
+
+        return this.channel.then((channel) => {
+            return initialiseQueue(queue, this.exchange, channel)
+                .then(() => sendToQueue(queue, JSON.stringify(message), channel))
+        }).catch(console.error);
     }
 
     listen(queue: string): Observable<any> {
         return Observable.fromPromise(this.channel)
-            .switchMap(consume.bind(null, queue))
+            .switchMap(channel => {
+                return initialiseQueue(queue, this.exchange, channel).then(() => consume(queue, channel))
+            })
             .filter(val => Boolean(val))
             .catch((err: Error) => {
                 console.error(err);
