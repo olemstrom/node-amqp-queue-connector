17dc37a04c67f2f86b94491cbd6dac7d56d9ac8c
Oscar Lemström <oscar.lemstrom@gmail.com>
2017-03-16 18:30:01 +0200

    Setup listening more sanely

--- main.d.ts
+++ main.d.ts
@@ -4,10 +4,17 @@ export interface ExchangeOptions {
     name: string;
     type: ExchangeType;
 }
+export interface MessageInfo {
+    content: string;
+    topic: string;
+}
 export declare class AmqpConnector {
     private channel;
     private exchange;
-    constructor(url: string, exchange?: ExchangeOptions);
-    publish(queue: string, message: any): Promise<any>;
-    listen(queue: string): Observable<any>;
+    private queue;
+    private listener;
+    constructor(url: string, queue: string, exchange?: ExchangeOptions);
+    publish(topic: string, message: any): Promise<any>;
+    listen(topic: string): Observable<any>;
+    private initialiseListen;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,14 @@ import { Channel, Message } from 'amqplib'
 import { BehaviorSubject, Subject, Observable } from 'rxjs/Rx'
 
 const initialiseQueue = (queue: string, exchange: string, channel: Channel) => Promise.all([
-    channel.assertQueue(queue),
-    channel.bindQueue(queue, exchange,'*')
+    channel.assertQueue(queue)
 ]);
 
-const sendToQueue = (queue: string, message: any, channel: Channel) => channel.sendToQueue(queue, new Buffer(message));
-
-const consume = (topic: string, channel: Channel) => {
-    const sub: Subject<any> = new BehaviorSubject(undefined);
-
-    channel.assertQueue(topic).then(ok => channel.consume(topic, (msg: Message) => {
-        if(msg) {
-            sub.next(msg.content.toString());
-            channel.ack(msg);
-        }
-    }));
-
-    return sub.asObservable();
-};
+const publishTo = (
+    exchange: string,
+    topic: string,
+    message: any,
+    channel: Channel) => channel.publish(exchange, topic, new Buffer(message));
 
 export type ExchangeType = 'direct' | 'fanout' | 'topic';
 
@@ -29,39 +19,64 @@ export interface ExchangeOptions {
     type: ExchangeType;
 }
 
+export interface MessageInfo {
+    content: string;
+    topic: string;
+};
+
 export class AmqpConnector {
     private channel: Promise<Channel>;
     private exchange: string;
+    private queue: string;
+    private listener: Subject<MessageInfo> = new BehaviorSubject(undefined);
 
-    constructor(url: string, exchange: ExchangeOptions = { name: 'default-fanout', type: 'fanout'} ) {
+    constructor(url: string, queue: string, exchange: ExchangeOptions = { name: 'default-fanout', type: 'fanout'} ) {
+        this.exchange = exchange.name;
+        this.queue = queue;
+
         this.channel = amqp.connect(url)
             .then(c => c.createChannel())
             .then(channel => channel.assertExchange(exchange.name, exchange.type).then(() => channel))
+            .then(channel => initialiseQueue(queue, exchange.name, channel).then(() => channel))
+            .then(channel => {
+                this.initialiseListen(this.queue, channel);
+                return channel;
+            })
             .catch((error: Error) => {
-                throw error
+                console.error(error);
             });
 
-        this.exchange = exchange.name;
     }
 
-    publish(queue: string, message: any): Promise<any> {
-        const send = sendToQueue.bind(null, queue, JSON.stringify(message));
-
-        return this.channel.then((channel) => {
-            return initialiseQueue(queue, this.exchange, channel)
-                .then(() => sendToQueue(queue, JSON.stringify(message), channel))
-        }).catch(console.error);
+    publish(topic: string, message: any): Promise<any> {
+        return this.channel
+            .then((channel) => publishTo(this.exchange, topic, JSON.stringify(message), channel))
+            .catch(console.error);
     }
 
-    listen(queue: string): Observable<any> {
-        return Observable.fromPromise(this.channel)
-            .switchMap(channel => {
-                return initialiseQueue(queue, this.exchange, channel).then(() => consume(queue, channel))
-            })
+    listen(topic: string): Observable<any> {
+        return Observable.fromPromise(
+                this.channel.then(channel => channel.bindQueue(this.queue, this.exchange, topic))
+            )
+            .switchMap(() => this.listener.asObservable())
             .filter(val => Boolean(val))
+            .filter((message) => message.topic === topic)
+            .map(message => message.content)
             .catch((err: Error) => {
                 console.error(err);
                 return Observable.throw(err);
             });
     }
+
+    private initialiseListen = (queue: string, channel: Channel): Observable<MessageInfo> => {
+        channel.assertQueue(queue)
+            .then(() => channel.consume(queue, (msg: Message) => {
+                if(msg) {
+                    this.listener.next({ content: msg.content.toString(), topic: msg.fields.routingKey });
+                    channel.ack(msg);
+                }
+        }));
+
+        return this.listener.asObservable();
+    };
 }
